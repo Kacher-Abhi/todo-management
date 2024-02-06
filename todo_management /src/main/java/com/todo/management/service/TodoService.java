c1aaa344b410dbd97111ffd5f7c37ec38f6ea84a
Kacher-Abhi <abhishekbkacher@gmail.com>
2024-02-06 23:18:29 +0530

    initial comit
    added : entity, controller, service, repo, dto

---
+++ todo_management /src/main/java/com/todo/management/service/TodoService.java
@@ -0,0 +1,24 @@
+package com.todo.management.service;
+
+
+
+import java.util.List;
+
+import com.todo.management.dto.TodoDto;
+
+public interface TodoService {
+
+    TodoDto addTodo(TodoDto todoDto);
+
+    TodoDto getTodo(Long id);
+
+    List<TodoDto> getAllTodos();
+
+    TodoDto updateTodo(TodoDto todoDto, Long id);
+
+    void deleteTodo(Long id);
+
+    TodoDto completeTodo(Long id);
+
+    TodoDto inCompleteTodo(Long id);
+}
