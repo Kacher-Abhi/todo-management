ddf68007f936023fe959ce65aad76e4f8bf973b7
Kacher-Abhi <abhishekbkacher@gmail.com>
2025-02-07 20:01:15 +0530

    Added new entities and yml config

---
+++ backend/todo-app/src/main/java/com/todo/app/entity/Task.java
@@ -0,0 +1,39 @@
+package com.todo.app.entity;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Task {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	private UUID id;
+	
+	private String shortDesc;
+	
+	private String longDesc;
+
+	private LocalDate createdDate;
+	
+	private LocalTime createdTime;
+	
+	private LocalDate endDate;
+	
+	private LocalTime endTime;
+
+	private String status = "in-progress";
+	
+}
